727524d5ce4a24e152db1429b9d96ad55544b3a2
Cmmmmmm <512065439@qq.com>
2025-01-05 09:51:00 +0000

    Initial commit

---
+++ Patcher.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using PerfectRandom.Sulfur.Core;
+using PerfectRandom.Sulfur.Core.Items;
+using PerfectRandom.Sulfur.Core.Units;
+using PerfectRandom.Sulfur.Core.Weapons;
+using UnityEngine;
+
+namespace ExpShare;
+
+public class Patcher {
+    [HarmonyPostfix, HarmonyPatch(typeof(Player), "Start")]
+    private static void PlayerStartPatch(Player __instance) {
+        __instance.playerUnit.gameObject.AddComponent<InfoPrinter>();
+    }
+
+    [HarmonyPostfix, HarmonyPatch(typeof(Npc), "GiveExperience")]
+    private static void GiveExperiencePrePatch(Npc __instance) {
+        var lastUsedWeapon = StaticInstance<GameManager>.Instance.GetPlayerUnit().lastUsedWeapon;
+        var secondWeapon = lastUsedWeapon.inventorySlot == InventorySlot.Weapon0
+            ? InventorySlot.Weapon1
+            : InventorySlot.Weapon0;
+        var equippedWeapon = StaticInstance<GameManager>.Instance.GetPlayerUnit().GetComponent<EquipmentManager>()
+            .EquippedHoldables;
+        
+        float exp = __instance.ExperienceOnKill;
+        var proportion = Plugin.Proportion.Value;
+        if (!equippedWeapon.ContainsKey(secondWeapon)) return;
+        equippedWeapon[secondWeapon].AddExperience(exp * proportion);
+        
+        if (!Plugin.ShowInfo.Value) return;
+        StaticInstance<GameManager>.Instance.GetPlayerUnit().GetComponent<InfoPrinter>().AddMessage($"Shared experience between weapons: {exp}({exp * proportion})", 10);
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ExpShare;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    
+    internal static ConfigEntry<float> Proportion;
+    internal static ConfigEntry<bool> ShowInfo;
+
+    private void Awake() {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loading!");
+
+        // Harmony patching
+        Harmony.CreateAndPatchAll(typeof(Patcher));
+        
+        // Config
+        Proportion = this.Config.Bind("General", "Proportion", 0.1f, "The proportion of the exp that another weapon will received");
+        ShowInfo = this.Config.Bind("General", "ShowInfo", true, "Print the experience share info");
+        
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
