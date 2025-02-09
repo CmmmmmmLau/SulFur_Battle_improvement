6c48165c4885f5e34e1b1fdb895fa15ac8c5c235
Cmmmmmm <512065439@qq.com>
2025-02-09 10:35:35 +0000

    imple: loot particle

---
+++ Patcher/QOL/LootParticlePatch.cs
@@ -0,0 +1,16 @@
+﻿using BattleImprove.Components;
+using BattleImprove.Utils;
+using HarmonyLib;
+using PerfectRandom.Sulfur.Core.World;
+using UnityEngine;
+
+namespace BattleImprove.Patcher;
+
+
+public class LootParticlePatch {
+    [HarmonyPostfix, HarmonyPatch(typeof(Pickup), "Awake")]
+    private static void Postfix(ref Pickup __instance) {
+        Plugin.instance.LoggingInfo("LootParticle Postfix", true);
+        Prefab.LoadLootParticlePrefab(__instance.gameObject);
+    }
+}
