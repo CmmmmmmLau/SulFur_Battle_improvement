c209f0fb6dc64f620bb960d11ac85ca4416103f1
Cmmmmmm <512065439@qq.com>
2025-02-13 16:42:06 +0000

    impl: loot drop vfx

---
+++ Components/QOL/LootDropVFX.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using BattleImprove;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LootDropVFX : MonoBehaviour{
+    public ParticleSystem[] systems;
+    private GameObject parentObject;
+    private bool isScaling;
+    private void Start() {
+        this.transform.localPosition = new Vector3(0, 0.01f, 0);
+        this.isScaling = true;
+        StartCoroutine(StopScale());
+    }
+
+    private void Update() {
+        bool isPressed = Input.GetMouseButton(1);
+        foreach (var system in systems) {
+            system.gameObject.SetActive(!isPressed);
+        }
+        
+        if (!isScaling) return;
+        this.gameObject.transform.localScale = parentObject.transform.localScale;
+        foreach (var system in systems) {
+            var parentScale = parentObject.transform.localScale;
+            system.transform.localScale = new Vector3(1 / parentScale.x, 1 / parentScale.y, 1 / parentScale.z);
+        }
+    }
+
+    private IEnumerator StopScale() {
+        yield return new WaitForSeconds(3);
+        this.isScaling = false;
+    }
+
+    public void SetParent(GameObject parent) {
+        parentObject = parent;
+    }
+}
--- Patcher/QOL/LootParticlePatch.cs
+++ Patcher/QOL/LootParticlePatch.cs
@@ -1,16 +1,18 @@
-﻿using BattleImprove.Components;
-using BattleImprove.Utils;
+﻿using BattleImprove.Utils;
 using HarmonyLib;
 using PerfectRandom.Sulfur.Core.World;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
-namespace BattleImprove.Patcher;
-
-
+namespace BattleImprove.Patcher.QOL;
 public class LootParticlePatch {
     [HarmonyPostfix, HarmonyPatch(typeof(Pickup), "Awake")]
     private static void Postfix(ref Pickup __instance) {
         Plugin.instance.LoggingInfo("LootParticle Postfix", true);
-        Prefab.LoadLootParticlePrefab(__instance.gameObject);
+        var shadow = __instance.transform.Find("Shadow").gameObject;
+        Object.Destroy(shadow.GetComponent<DecalProjector>());
+        var lootdrop = Prefab.LoadPrefab("LoopDropTier1", __instance.gameObject);
+        var components = lootdrop.GetComponent<LootDropVFX>();
+        components.SetParent(shadow);
     }
 }
