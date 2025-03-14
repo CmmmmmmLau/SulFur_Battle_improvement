19947a3e97f287b9488bec15fe3dacd827787e04
Cmmmmmm <512065439@qq.com>
2025-03-14 12:40:31 +0000

    fix: v0.10.0 compatibility fix
    Close #4

--- Transpiler/RemoveDeadBodyCollisionTranspiler.cs
+++ Transpiler/RemoveDeadBodyCollisionTranspiler.cs
@@ -11,6 +11,10 @@ using UnityEngine;
 namespace BattleImprove.Transpiler;
 
 public class RemoveDeadBodyCollisionTranspiler {
+#if DEBUG
+    private static CodeMatcher codeMatcher;
+#endif
+    
     [HarmonyTranspiler,HarmonyPatch(typeof(Projectile), "HandleHit")]
     private static IEnumerable<CodeInstruction> ProjectileTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator, MethodBase original) {
         var codeMatcher = new CodeMatcher(instructions).End();
@@ -22,20 +26,25 @@ public class RemoveDeadBodyCollisionTranspiler {
         // Match the target code in Line:201 → if (this.fHitsLeft <= 0)
         codeMatcher.MatchBack(true, new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(PerfectRandom.Sulfur.Core.Weapons.Projectile), "fHitsLeft")));
 
-        // Push hitbox to the stack first
-        // and insert new instruction code with the label
-        var insertCode = new[] {
-            new CodeInstruction(OpCodes.Ldarg_2),
-            Transpilers.EmitDelegate(CheckHitboxAlive),  
-            new CodeInstruction(OpCodes.Brfalse_S, endLabel)
-        };
-        codeMatcher.Advance(3).InsertAndAdvance(insertCode); 
-        // The code now should be like this:
-        // if (this.fHitsLeft <= 0) {
-        //  if (BulletDirection(hitbox)) {
-        //      .......
-        //  }
-        // }
+        if (codeMatcher.IsValid) {
+            // Push hitbox to the stack first
+            // and insert new instruction code with the label
+            var insertCode = new[] {
+                new CodeInstruction(OpCodes.Ldarg_2),
+                Transpilers.EmitDelegate(CheckHitboxAlive),  
+                new CodeInstruction(OpCodes.Brfalse_S, endLabel)
+            };
+            codeMatcher.Advance(3).InsertAndAdvance(insertCode); 
+            // The code now should be like this:
+            // if (this.fHitsLeft <= 0) {
+            //  if (BulletDirection(hitbox)) {
+            //      .......
+            //  }
+            // }
+        } else {
+            Plugin.instance.LoggingInfo("Failed to find the target code in Projectile.HandleHit");
+        }
+
         
         
         return codeMatcher.InstructionEnumeration();
@@ -43,66 +52,73 @@ public class RemoveDeadBodyCollisionTranspiler {
     
     [HarmonyTranspiler,HarmonyPatch(typeof(ProjectileSystem), "Update")]
     private static IEnumerable<CodeInstruction> ProjectileSystemTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator, MethodBase original) {
-        var codeMatcher = new CodeMatcher(instructions).End();
+        // Define a variable to store the hitbox index
+        // !!! This variable must be checked in every game broken update !!!
+        var hitboxVariable = Convert.ToByte(11);
+        
+        codeMatcher = new CodeMatcher(instructions).End();
     
-        // Match the target code in Line: 175 → if (math.lengthsq(ptr.velocity) > 0.0001f)
+        // Match the target code in Line: 192 → ptr.displacement = 0f;
         // And add a jump label to this code
         var label1 = generator.DefineLabel();
         var match = new[] {
-            new CodeMatch(OpCodes.Ldloc_3),
-            new CodeMatch(OpCodes.Ldfld),
-            new CodeMatch(OpCodes.Call),
-            new CodeMatch(OpCodes.Ldc_R4)
+            new CodeMatch(OpCodes.Ldloc_2),
+            new CodeMatch(i => i.opcode == OpCodes.Ldc_R4 && i.operand.Equals(0f))
         };
-        codeMatcher.MatchBack(false,match).Instruction.labels.Add(label1);
+        codeMatcher.MatchBack(false,match)
+            .ThrowIfInvalid("Failed to find the target code in in Line: 192 -> ptr.displacement = 0f;")
+            .Instruction.WithLabels(label1);
         
-        // Match the target code in Line: 174  → ptr.velocity = Vector3.Reflect(ptr.velocity, raycastHit.normal) * num5;
+        // Match the target code in Line: 191  → ptr.velocity = Vector3.Reflect(ptr.velocity, raycastHit.normal) * num4;
         match = new[] {
-            new CodeMatch(OpCodes.Ldloc_3),
-            new CodeMatch(OpCodes.Ldloc_3)
+            new CodeMatch(OpCodes.Ldloc_2),
+            new CodeMatch(OpCodes.Ldloc_2)
         };
-        codeMatcher.MatchBack(false,match);
-
-        // Copy the label from the target code
-        var labels = codeMatcher.Instruction.labels;
+        
         // Push hitbox to the stack first
         // and insert new instruction code with the label
         var instructionsList = new[] {
-            new CodeInstruction(OpCodes.Ldloc_S, Convert.ToByte(10)).WithLabels(labels),
+            new CodeInstruction(OpCodes.Ldloc_S, hitboxVariable),
             Transpilers.EmitDelegate(CheckHitboxAlive),
             new CodeInstruction(OpCodes.Brfalse_S, label1)
         };
-        // Dont forget to clear the label that only original code has
-        codeMatcher.InsertAndAdvance(instructionsList).Labels.Clear();
+        
         // The code now should be like this:
         // if (BulletDirection(hitbox)) {
-        //  ptr.velocity = Vector3.Reflect(ptr.velocity, raycastHit.normal) * num5;
+        //  ptr.velocity = Vector3.Reflect(ptr.velocity, raycastHit.normal) * num4;
         // }
+        codeMatcher.MatchBack(false,match)
+            .ThrowIfInvalid("Failed to find the target code in Line: 191 -> ptr.velocity = Vector3.Reflect(ptr.velocity, raycastHit.normal) * num4;")
+            .Insert(instructionsList);
         
-        // Match the target code  in Line: 142 → ptr.position = raycastHit.point;
+        // Match the target code  in Line: 148 → ptr.position = raycastHit.point;
         // And do the same thing as above
         // The code now should be like this:
         // if (BulletDirection(hitbox)) {
         //  ptr.position = raycastHit.point;
         // }
+        
+        // 1. Add label fisrt
         var label2 = generator.DefineLabel();
         match = new[] {
-            new CodeMatch(OpCodes.Ldloc_3),
+            new CodeMatch(OpCodes.Ldloc_2),
             new CodeMatch(OpCodes.Ldloca_S),
             new CodeMatch(OpCodes.Call),
             new CodeMatch(OpCodes.Call),
             new CodeMatch(OpCodes.Stfld),
-            new CodeMatch(OpCodes.Ldloc_S),
+            new CodeMatch(OpCodes.Ldloc_3),
         };
-        codeMatcher.MatchBack(true, match).Labels.Add(label2);
-        labels = codeMatcher.MatchBack(false, match[0]).Labels;
+        codeMatcher.MatchBack(true, match)
+            .ThrowIfInvalid("Failed to find the target code in Line: 148 -> ptr.position = raycastHit.point;")
+            .Instruction.WithLabels(label2);
         
         instructionsList = new[] {
-            new CodeInstruction(OpCodes.Ldloc_S, Convert.ToByte(10)).WithLabels(labels),
+            new CodeInstruction(OpCodes.Ldloc_S, hitboxVariable).MoveLabelsFrom(codeMatcher.MatchBack(false, match[0]).Instruction),
             Transpilers.EmitDelegate(CheckHitboxAlive),
             new CodeInstruction(OpCodes.Brfalse_S, label2)
         };
-        codeMatcher.InsertAndAdvance(instructionsList).Labels.Clear();
+        
+        codeMatcher.Insert(instructionsList);
         
         return codeMatcher.InstructionEnumeration();
     }
