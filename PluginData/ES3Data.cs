14f448f0ceb869e469b57ea47dfcceb97cb36d54
Cmmmmmm <512065439@qq.com>
2025-07-03 21:45:13 +0100

    fix: update ES3Data to use customizable settings for loading and saving

--- PluginData/ES3Data.cs
+++ PluginData/ES3Data.cs
@@ -1,18 +1,19 @@
 ﻿namespace BattleImprove.PluginData;
 
 public abstract class ES3Data<T> where T: class, new() {
+    private static readonly ES3Settings settings = new ES3Settings("BattleImprove.es3");
     private static readonly string key = typeof(T).Name;
     private static T instance;
     public static T Instance {
         get {
             if (instance == null) {
-                instance = ES3.KeyExists(key)? ES3.Load<T>(key) : new T();
+                instance = ES3.KeyExists(key, settings)? ES3.Load<T>(key, settings) : new T();
             }
             return instance;
         }
     }
     
     public static void Save() {
-        ES3.Save(key, instance, "BattleImprove");
+        ES3.Save(key, instance, settings);
     }
 }
